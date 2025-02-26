ad2e0a403fe7fcaa6e40ac1b597ff510e7770837
SandeshHiubGit <sandeshgaikwad1994@gmail.com>
2025-02-26 14:23:33 +0530

    added file on main-hostfix branch this is from main branch

---
+++ mainHostFix.py
@@ -0,0 +1,20 @@
+
+## This is aadming hot fix frile from main branch in main-hotfix
+
+
+## unordered
+## key value paire 
+
+# You can define dictionory in two way 
+my_dist = {"name":"sandesh", "age": 26, "bmi": 19.5, "code": "python"}
+
+print(my_dist["age"]) ## You can call a key it will give the value of key like age is key and 26 is value.
+print(my_dist)
+my_dist = {
+            "name":"sandesh",
+           "age": 26,
+            "bmi": 19.5,
+            "code": "python"
+         }
+print(my_dist["age"]) ## pass the key name here 
+print(my_dist)
