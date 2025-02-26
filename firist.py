913b3408159a6580c4074eaebddd2296a33b7e1c
SandeshHiubGit <sandeshgaikwad1994@gmail.com>
2025-02-26 13:50:20 +0530

    files from main branch

---
+++ firist.py
@@ -0,0 +1,13 @@
+print("Hello World")
+i = 0
+if i > 10:
+    print ("hello")
+    i = i + 1
+else:
+    print("no hello")
+
+print("after collecbator")
+
+# from main user again add file 
+
+## AFter adding user in global file 
